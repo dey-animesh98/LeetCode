f65e98ca6443bdb3c38a746f432f29910d9ac963
Animesh Dey <animesh.dey986@gmail.com>
2022-06-11 10:21:22 +0530

    plus one

--- Easy/27. Remove Element.js
+++ Easy/27. Remove Element.js
@@ -52,3 +52,14 @@ Constraints:
 */
 
 //-
+{
+    var removeElement = function(nums, val) {
+    
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] === val) {
+               nums.splice(i,1);
+               i-=1;
+            }
+        }
+    };
+}
