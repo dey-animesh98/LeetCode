6e0fc62126526161e0a554a08279d7887ea303bd
Animesh Dey <animesh.dey986@gmail.com>
2022-06-13 22:03:04 +0530

    35

--- Easy/35. Search Insert Position.js
+++ Easy/35. Search Insert Position.js
@@ -57,3 +57,22 @@ nums contains distinct values sorted in ascending order.
         return start
     };
 }
+///////////////////////////////////////////////////////////
+{
+    function largestOfFour(arr) {
+        let newArr = []
+        for(let i=0;i<arr.length;i++){
+             let maxsum= 1/-0
+         for(let j =0;j<arr[i].length;j++){
+           if(maxsum<arr[i][j])
+             maxsum = arr[i][j]
+         }
+         newArr.push(maxsum)
+        }
+        return newArr
+      }
+      largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+      
+}
+
+//mergeSort(n log n), bubble sort
